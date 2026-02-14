fb370bb33bd75a533e3115717876b99296ba3bad
Dipannita Sharma <dipannitasharma1@gmail.com>
2026-02-14 23:21:14 +0530

    feat: update QRForm component to accept qrData and setQrData props

--- src/components/QRForm.jsx
+++ src/components/QRForm.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const QRForm = () => {
+const QRForm = ({qrData, setQrData}) => {
   return (
-    <div>QRForm</div>
+    <div className='w-full max-w-md mt-8 text-white'>
+        <input type="text"
+        value={qrData}
+        onChange={(e) => setQrData(e.target.value)}
+        placeholder="Enter the text/url"
+        className="p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+    </div>
   )
 }
 
