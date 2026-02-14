d1936c6b64a809360739e0a077a7aa1ccd1c7e85
Dipannita Sharma <dipannitasharma1@gmail.com>
2026-02-14 23:16:41 +0530

    feat: add QRForm component and integrate with App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Header";
+import QRForm from "./components/QRForm";
 
 const App = () => {
+  const[qrData, setQrData] = useState("");
   return (
     <div className="min-h-screen 
                     bg-linear-to-br 
                     from-gray-900 via-black to-gray-800
                     flex flex-col items-center">
       <Header />
+          <div><QRForm qrData={qrData} setQrData={setQrData} /></div>
     </div>
   );
 };
---
+++ src/components/QRForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const QRForm = () => {
+  return (
+    <div>QRForm</div>
+  )
+}
+
+export default QRForm
